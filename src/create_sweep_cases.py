31a79b5891c70d01bbf9539bc243b1e18681ad85
Shirley Yu <shirleyxy.yu@gmail.com>
2025-03-13 19:16:31 -0700

    fixed json overriding issue with sweep

--- src/create_sweep_cases.py
+++ src/create_sweep_cases.py
@@ -8,13 +8,14 @@ def count_decimals(number):
     decimal_part = number_string.split('.')[1]
     return len(decimal_part)
 
-def getRate(step_size):
+def getRate(start, end, step_size):
     rate = []
-    for i in np.arange(0.8, 1, step_size): 
+    for i in np.arange(start, end, step_size): 
         rate.append(round(i, count_decimals(step_size)))
     
     return rate
 
+# these are the number of layers for each model
 models = {'roberta': 12, 'gptj': 28, 'llama2': 32}
 def getLayer(model): 
     layers = []
@@ -29,10 +30,10 @@ lnames = ['fc_in', 'fc_out']
 
 try:
     # Open and read the JSON file
-    with open('sweep.json', 'r+') as file:
-        data = json.load(file)
+    with open('sweep.json', 'w+') as file:
+        data = {}
         data["lname"] = lnames
-        data["rate"] = getRate(0.01)
+        data["rate"] = getRate(0.8, 1, 0.01)
         data["lnum"] = getLayer('gptj')
         json_output = json.dumps(data, indent=4)
         print(json_output) 
